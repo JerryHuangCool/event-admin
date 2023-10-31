963d2a4da4d9c1bfbbf46c29735f9ddd0656c4f1
JerryHuangCool <905056897@qq.com>
2023-10-31 17:50:20 +0800

    '个人信息'

--- src/api/user.js
+++ src/api/user.js
@@ -5,3 +5,29 @@ export function getUserInfo() {
     url: '/my/userinfo'
   })
 }
+
+export function updateUserInfo(data) {
+  return request({
+    url: '/my/userinfo',
+    method: 'put',
+    data
+  })
+}
+
+export function updateAvatar(avatar) {
+  return request({
+    url: '/my/update/avatar',
+    method: 'patch',
+    data: {
+      avatar
+    }
+  })
+}
+
+export function updatePassword(data) {
+  return request({
+    url: '/my/updatepwd',
+    method: 'patch',
+    data
+  })
+}
